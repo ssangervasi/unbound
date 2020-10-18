3471f56cfa1fedbd458d444109c92a7c72dae478
Sebastian Sangervasi <villain@harmless.dev>
2020-10-18 15:22:00 -0700

    Licensing

---
+++ Fonts/basty/font_scale_calcs.py
@@ -0,0 +1,29 @@
+
+def sprite_to_font():
+	print('sprite_to_font')
+	initial = (4, 6)
+	target = (533, 800)
+	scale_perc = 13333
+	move = (498, -397)
+	print(move)
+
+def cap_to_lower():
+	print('cap_to_lower')
+	cap_h = 800.0
+	cap_px_h = cap_h/6
+	low_h = cap_px_h * 5
+	low_px_h = low_h / 6
+
+	shift_px = cap_px_h / 2 
+	scale_perc = 100 * low_h / cap_h 
+	print({
+	 "shift_px": shift_px,
+	 "scale_perc": scale_perc,	
+	})
+
+def normal_to_tiny():
+	print(
+		100 * 1.0/133
+	)
+
+normal_to_tiny()
---
+++ Fonts/basty/letters_import.py
@@ -0,0 +1,80 @@
+import string
+
+import fontforge
+
+
+def main():
+    print("Opening font")
+    filename = "Basty.sfd"
+    font = fontforge.open(filename)
+
+    # import_letters(font)
+    import_numbers(font)
+    import_symbols(font)
+
+    font.save(filename)
+
+
+def import_letters(font):
+    for c in string.ascii_uppercase:
+        print(f"Importing letter: {c}")
+        import_glyph(font, c)
+
+
+def import_numbers(font):
+    for n in map(str, range(0, 10)):
+        print(f"Importing number: {n}")
+        import_glyph(font, n)
+
+
+def import_symbols(font):
+    # Lol why did I use my own names instead of the standard ones
+    sym_to_alias = {
+        "!": "exclamation",
+        '"': "quote_double",
+        "'": "quote_single",
+        "#": "number",
+        "$": "dollar",
+        "%": "percent",
+        "&": "ampersand",
+        "(": "paren_open",
+        ")": "paren_close",
+        "*": "star",
+        "+": "plus",
+        ",": "comma",
+        "-": "dash",
+        ".": "dot",
+        "/": "slash",
+        ":": "colon",
+        ";": "slash",
+        "<": "less_than",
+        "=": "equal",
+        ">": "greater_than",
+        "?": "question",
+        "@": "at",
+        "[": "bracket_open",
+        "]": "bracket_close",
+        "\\": "backslash",
+        "^": "caret",
+        "_": "underscore",
+        "`": "backtick",
+        "{": "brace_open",
+        "}": "brace_close",
+        "|": "bar",
+        "~": "tilde",
+    }
+    for s, alias in sym_to_alias.items():
+        print(f"Importing symbol: {s} as {alias}")
+        import_glyph(font, s, file_alias=alias)
+
+
+def import_glyph(font, glyph_name, file_alias=None):
+    if file_alias is None:
+        file_alias = glyph_name
+
+    glyph = font.createMappedChar(glyph_name)
+    glyph.importOutlines(f"letters/BastyFont ({file_alias}).svg")
+
+
+if __name__ == "__main__":
+    main()
